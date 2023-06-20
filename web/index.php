5c617ff49ab6637abc00828c013b658cdfe0a201
Мишин Артём <artem.mishin@rau.ru>
2023-06-20 20:32:59 +0300

    index refactor

--- web/index.php
+++ web/index.php
@@ -1,12 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG', $config['yii_debug'] ?? false);
+defined('YII_ENV') or define('YII_ENV', $config['yii_env'] ?? 'dev');
+
 error_reporting(0);
+ini_set('memory_limit', '-1');
+
 (new yii\web\Application($config))->run();
