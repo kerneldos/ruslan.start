19a73fa47416541864cbcbde3b0b5e626b14654e
Мишин Артём <artem.mishin@rau.ru>
2023-06-20 20:34:33 +0300

    env default prod

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 $config = require __DIR__ . '/../config/web.php';
 
 defined('YII_DEBUG') or define('YII_DEBUG', $config['yii_debug'] ?? false);
-defined('YII_ENV') or define('YII_ENV', $config['yii_env'] ?? 'dev');
+defined('YII_ENV') or define('YII_ENV', $config['yii_env'] ?? 'prod');
 
 error_reporting(0);
 ini_set('memory_limit', '-1');
