ecf208ca81a9aa4adecc027a4cdea00480f5660d
Мишин Артём <artem.mishin@rau.ru>
2023-12-13 20:18:28 +0300

    consumer config

--- console/views/new-consumer/config.php
+++ console/views/new-consumer/config.php
@@ -5,7 +5,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=mysql;port=<?= Yii::$app['params']['db_port'] ?>;dbname=consumer_<?= $consumer ?>',
+            'dsn' => 'mysql:host=mysql;port=<?= Yii::$app->params['db_port'] ?>;dbname=consumer_<?= $consumer ?>',
             'username' => <?= Yii::$app->params['db_user'] ?>,
             'password' => <?= Yii::$app->params['db_password'] ?>,
             'charset' => 'utf8',
