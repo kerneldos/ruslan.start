4a01ef2f92e5093c434f3ace53c96f9b2e6cb706
Мишин Артём <artem.mishin@rau.ru>
2024-03-24 16:03:11 +0300

    chart widgets fixes

--- consumer/modules/dashboard/widgets/views/documents-by-date.php
+++ consumer/modules/dashboard/widgets/views/documents-by-date.php
@@ -12,7 +12,7 @@
     </div><!-- /.card-header -->
     <div class="card-body">
         <div class="tab-content p-0 chart-container">
-            <canvas data-chart-data=<?= json_encode($chartData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?> height="300" style="height: 300px;"></canvas>
+            <canvas data-chart-data='<?= json_encode($chartData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>' height="300" style="height: 300px;"></canvas>
         </div>
     </div><!-- /.card-body -->
 </div>
